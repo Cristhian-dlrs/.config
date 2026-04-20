d46f61d5a58ec05606b8f83c75ca96e957fa03c6
Cristhian-dlrs <christian.dlr.ing@gmail.com>
2026-04-20 14:02:54 -0400

    add deps.txt and msc to lua line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,28 +1,31 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require("lualine").setup({
-            -- Shows notifications like recording macro in lualine
-            sections = {
-                lualine_x = {
-                    {
-                        require("noice").api.statusline.mode.get,
-                        cond = require("noice").api.statusline.mode.has,
-                        color = { fg = "#fff" },
-                    },
-                    "encoding",
-                    "fileformat",
-                    "filetype",
-                },
-            },
-            options = {
-                theme = "catppuccin-mocha",
-                sections = {
-                    lualine_a = {
-                        file = 1,
-                    },
-                },
-            },
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			-- Shows notifications like recording macro in lualine
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#fff" },
+					},
+					-- "encoding",
+					-- "fileformat",
+					"filetype",
+				},
+			},
+			options = {
+
+				theme = "catppuccin-mocha",
+				sections = {
+					lualine_a = {
+						file = 1,
+					},
+				},
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+		})
+	end,
 }
